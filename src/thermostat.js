ec2f3345d72951852b15f0c242da93400a85653a
Simone <simonecaplin@gmail.com>
2018-06-14 09:44:06 +0100

    Set upper limits with power saving mode

--- src/thermostat.js
+++ src/thermostat.js
@@ -3,6 +3,8 @@
 function Thermostat() {
   this.powerSavingMode = true;
   this.MINIMUM_TEMPERATURE = 10;
+  this.MAX_LIMIT_PSM_ON = 25;
+  this.MAX_LIMIT_PSM_OFF = 32;
   this.temperature = 20;
 };
 
@@ -11,6 +13,9 @@ Thermostat.prototype.getCurrentTemperature = function(){
 };
 
 Thermostat.prototype.up = function() {
+  if(this.isMaximumTemperature()) {
+    return;
+  }
   this.temperature += 1;
 };
 
@@ -24,12 +29,22 @@ Thermostat.prototype.down = function() {
 Thermostat.prototype.isMinimumTemperature = function() {
   return this.temperature === this.MINIMUM_TEMPERATURE;
 };
-Thermostat.prototype.isPowerSavingModeOn = function () {
+
+Thermostat.prototype.isMaximumTemperature = function() {
+  if (this.isPowerSavingModeOn() === false) {
+    return this.temperature === this.MAX_LIMIT_PSM_OFF;
+  }
+  return this.temperature === this.MAX_LIMIT_PSM_ON;
+};
+
+Thermostat.prototype.isPowerSavingModeOn = function() {
   return this.powerSavingMode === true;
 };
-Thermostat.prototype.switchPowerSavingModeOff = function () {
+
+Thermostat.prototype.switchPowerSavingModeOff = function() {
   this.powerSavingMode = false;
-}
-Thermostat.prototype.switchPowerSavingModeOn = function () {
+};
+
+Thermostat.prototype.switchPowerSavingModeOn = function() {
   this.powerSavingMode = true;
-}
+};
